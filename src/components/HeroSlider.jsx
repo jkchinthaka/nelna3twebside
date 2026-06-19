259834b6a5f8d9c5b8ef930cfb711d4bb513a68f
Chinthaka Jayaweera <chinthakajayaweera1@gmail.com>
2026-06-19 14:26:08 +0530

    okk

--- src/components/HeroSlider.jsx
+++ src/components/HeroSlider.jsx
@@ -1,17 +1,10 @@
 import AchievementStatsBar from './AchievementStatsBar.jsx'
-import BrandChickenMascot from './BrandChickenMascot.jsx'
 import HeroCarousel from './HeroCarousel.jsx'
 
 function HeroSlider() {
   return (
     <section className="hero-section min-h-[100svh]" aria-label="Nelna Farm visual showcase">
       <HeroCarousel />
-      <BrandChickenMascot
-        size="sm"
-        variant="float"
-        animate
-        className="hero-section__mascot"
-      />
       <AchievementStatsBar />
     </section>
   )
